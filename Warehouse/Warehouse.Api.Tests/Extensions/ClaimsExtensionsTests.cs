e54573baa1f50a5b2210d5b034591429c21c1475
MichaelDiers <md@sreid.de>
2023-11-04 08:11:23 +0100

    add tests

---
+++ Warehouse/Warehouse.Api.Tests/Extensions/ClaimsExtensionsTests.cs
@@ -0,0 +1,43 @@
+﻿namespace Warehouse.Api.Tests.Extensions
+{
+    using System.Security.Claims;
+    using Warehouse.Api.Contracts;
+    using Warehouse.Api.Extensions;
+
+    /// <summary>
+    ///     Tests for <see cref="ClaimsExtensions" />.
+    /// </summary>
+    public class ClaimsExtensionsTests
+    {
+        [Theory]
+        [InlineData(false)]
+        [InlineData(
+            false,
+            ClaimTypes.Name,
+            ClaimTypes.Actor)]
+        [InlineData(
+            true,
+            ClaimTypes.Name,
+            CustomClaims.IdClaim)]
+        public void RequiredIdFails(bool success, params string[] claimTypes)
+        {
+            var claims = claimTypes.Select(
+                    ct => new Claim(
+                        ct,
+                        Guid.NewGuid().ToString()))
+                .ToArray();
+
+            if (success)
+            {
+                var id = claims.RequiredId();
+                Assert.Equal(
+                    claims.First(x => x.Type == CustomClaims.IdClaim).Value,
+                    id);
+            }
+            else
+            {
+                Assert.Throws<InvalidOperationException>(() => claims.RequiredId());
+            }
+        }
+    }
+}
