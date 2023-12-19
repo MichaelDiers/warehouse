e891627540464f72529170f1200e51cb2c6e6c04
MichaelDiers <md@sreid.de>
2023-12-19 12:43:33 +0100

    update routes

--- warehouse-frontend/src/types/app-routes.enum.ts
+++ warehouse-frontend/src/types/app-routes.enum.ts
@@ -2,8 +2,8 @@ const enum AppRoutes {
     ERROR_LOST = '/lost',
     SIGN_IN = '/sign-in',
     SIGN_UP = '/sign-up',
-    STOCK_ITEM_LIST = '/stockItem-list',
-    STOCK_ITEM_CREATE = '/stockItem-item-create'
+    STOCK_ITEM_LIST = '/stockItems',
+    STOCK_ITEM_CREATE = '/stockItem/create'
   };
   
   export default AppRoutes;
