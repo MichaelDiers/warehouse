751a18d51dd78aeb21d11880b464b5a18ae25879
MichaelDiers <md@sreid.de>
2023-12-21 19:10:43 +0100

    prevent multiple refreshing of the access token

--- warehouse-frontend/src/app/api-slice.ts
+++ warehouse-frontend/src/app/api-slice.ts
@@ -13,6 +13,9 @@ import ILink from '../types/link';
 import ApplicationError from '../types/application-error';
 import selectText from '../text/text-selector';
 import { updateUserThunk } from './user-slice';
+import { Mutex, withTimeout } from 'async-mutex'
+
+const mutex = withTimeout(new Mutex(), 30000, new Error('Mutex timeout after 30000ms.'));
 
 interface IData {
   links: ILink[];
@@ -27,7 +30,7 @@ const baseQuery: BaseQueryFn<
   string | FetchArgs,
   unknown,
   FetchBaseQueryError
-> = async (args: string|FetchArgs, api: BaseQueryApi, extraOptions: any) => {
+> = async (args: string | FetchArgs, api: BaseQueryApi, extraOptions: any) => {
   const state = api.getState() as RootState;
 
   const query = fetchBaseQuery({
@@ -37,14 +40,14 @@ const baseQuery: BaseQueryFn<
 
       headers.set('Content-Type', 'application/json');
       headers.set('x-api-key', state.apiKey.current);
-      
+
       const token = extraOptions?.token;
       !!token && headers.set('Authorization', `Bearer ${token}`);
-      
+
       return headers;
     }
   });
-  
+
   return query(args, api, extraOptions);
 }
 
@@ -53,9 +56,17 @@ const baseQueryReauth: BaseQueryFn<
   unknown,
   FetchBaseQueryError
 > = async (args: FetchArgs, api: BaseQueryApi, extraOptions: any) => {
+  // check if the process acquired the mutex
+  const isMutexOwner = extraOptions?.isMutexOwner === true;
+
+  // wait if a different request refreshes the access token
+  !isMutexOwner && await mutex.waitForUnlock();
+
+  // if the given url is an urn then get the url via options requests;
+  // use the given url otherwise.
   const providedUrl: string = args.url;
   const url = providedUrl.toLocaleLowerCase().startsWith('urn:')
-    ? await getUrl(providedUrl as Urn, api)
+    ? await getUrl(providedUrl as Urn, api, false)
     : providedUrl;
 
   if (!url) {
@@ -66,47 +77,64 @@ const baseQueryReauth: BaseQueryFn<
 
   const adjustedArgs = { ...args, url }
 
+  // send the request using the adjusted url
   const response = await baseQuery(
     adjustedArgs,
     api,
-    {...extraOptions, token: state.user.current?.accessToken},
+    { ...extraOptions, token: state.user.current?.accessToken },
   );
 
+  // the response is not related to access token errors
+  // stop if it is the refresh process
   if (
-    !response.error 
-    || response.error.status !== 401 
-    || !state.user.current?.refreshToken) {
+    !response.error
+    || response.error.status !== 401
+    || !state.user.current?.refreshToken
+    || isMutexOwner) {
     return response;
   }
 
-  let refreshUrl;
-  try {
-    refreshUrl = await getUrl(Urn.AUTH_REFRESH, api);
-  } catch {
-    return response;
+  // refresh the access token if the process is not already started
+  if (!mutex.isLocked()) {
+    const releaseMutex = await mutex.acquire();
+
+    try {
+      const refreshUrl = await getUrl(Urn.AUTH_REFRESH, api, true);
+      const tokenResponse = await baseQuery({
+        url: refreshUrl,
+        method: 'POST'
+      },
+        api,
+        {
+          isMutexOwner: true,
+          token: state.user.current.refreshToken
+        });
+
+      if (tokenResponse.error) {
+        return response;
+      }
+
+      const { accessToken, refreshToken } = tokenResponse.data as ITokenResponse;
+      api.dispatch(updateUserThunk(accessToken, refreshToken));
+    } catch {
+      // refreshing the access token failed
+      // therefore return the original response
+      return response;
+    } finally {
+      releaseMutex();
+    }
   }
 
-  const tokenResponse = await baseQuery({
-    url: refreshUrl,
-    method: 'POST'
-  },
-  api,
-  {
-    token: state.user.current.refreshToken
-  });
+  // wait until the refresh process is finished
+  await mutex.waitForUnlock();
 
-  if (tokenResponse.error) {
-    return response;
-  }
-
-  const { accessToken, refreshToken } = tokenResponse.data as ITokenResponse;
-  api.dispatch(updateUserThunk(accessToken, refreshToken));
+  // resend the request
   return baseQuery(
     adjustedArgs,
     api,
     {
       ...extraOptions,
-      token: accessToken,
+      token: (api.getState() as RootState).user.current?.accessToken,
     },
   );
 }
@@ -118,8 +146,8 @@ const getUrlForUrnFromCache = (urn: Urn, api: BaseQueryApi): string | undefined 
   return link?.url;
 }
 
-const getUrlForUrnFromRequest = async (url: string, urn: Urn, api: BaseQueryApi): Promise<string> => {
-  const options = await baseQueryReauth({ url, method: 'OPTIONS' }, api, {});
+const getUrlForUrnFromRequest = async (url: string, urn: Urn, api: BaseQueryApi, isMutexOwner: boolean): Promise<string> => {
+  const options = await baseQueryReauth({ url, method: 'OPTIONS' }, api, { isMutexOwner });
   const linkData: IData = options.data as IData;
 
   if (options.error || !linkData.links) {
@@ -136,20 +164,20 @@ const getUrlForUrnFromRequest = async (url: string, urn: Urn, api: BaseQueryApi)
   return option.url;
 }
 
-const getUrlForUrn = async (url: string, urn: Urn, api: BaseQueryApi): Promise<string> => {
+const getUrlForUrn = async (url: string, urn: Urn, api: BaseQueryApi, isMutexOwner: boolean): Promise<string> => {
   const cachedUrl = getUrlForUrnFromCache(urn, api);
   if (cachedUrl) {
     return cachedUrl;
   }
 
-  return getUrlForUrnFromRequest(url, urn, api);
+  return getUrlForUrnFromRequest(url, urn, api, isMutexOwner);
 }
 
-const getUrl = async (urn: Urn, api: BaseQueryApi): Promise<string> => {
+const getUrl = async (urn: Urn, api: BaseQueryApi, isMutexOwner: boolean): Promise<string> => {
   const optionsUrn = `urn:${urn.split(':')[1]}:Options`;
-  const optionsUrl = await getUrlForUrn('/api/Options', optionsUrn as Urn, api);
+  const optionsUrl = await getUrlForUrn('/api/Options', optionsUrn as Urn, api, isMutexOwner);
 
-  return getUrlForUrn(optionsUrl, urn, api);
+  return getUrlForUrn(optionsUrl, urn, api, isMutexOwner);
 }
 
 /**
